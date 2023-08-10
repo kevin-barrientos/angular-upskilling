459ef905bac96422d8fbb195a2cd397394223bbb
Kevin Barrientos <kevin.barrientos@modmed.com>
2023-08-10 12:47:59 -0600

    sessions 05: guards todos route

---
+++ src/app/@features/auth/guard/auth.guard.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authorize();
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authorize();
+  }
+
+  private authorize() {
+    const user = this.userService.getUser();
+
+    if (user) {
+      return true;
+    } else {
+      return this.router.createUrlTree(['/login']);
+    }
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './@features/auth/login/login.component';
+import { AuthGuard } from './@features/auth/guard/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,6 +10,8 @@ const routes: Routes = [
   },
   {
     path: 'todos',
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
     loadChildren: () => import('./@features/todos/todos.module').then((m) => m.TodosModule),
   },
   { path: '**', redirectTo: '/todos' },
