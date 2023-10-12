77d8392c1c3df6c55cd39861189a30fbcba4d4cf
Kevin Barrientos <kevin.barrientos@modmed.com>
2023-10-11 23:20:09 -0600

    sessions 08: using ng-mocks 102

--- src/app/@features/auth/login/login.component.spec.ts
+++ src/app/@features/auth/login/login.component.spec.ts
@@ -1,15 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MockProvider, ngMocks } from 'ng-mocks';
+import { MockBuilder, MockInstance, MockProvider, MockRender } from 'ng-mocks';
+import { LoginModule } from './login.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -18,19 +14,21 @@ describe('LoginComponent', () => {
   let userServiceMock: jasmine.SpyObj<UserService>;
   let routerMock: jasmine.SpyObj<Router>;
 
-  beforeEach(async () => {
+  MockInstance.scope();
+
+  beforeEach(() => {
     userServiceMock = jasmine.createSpyObj(['setUser']);
     routerMock = jasmine.createSpyObj(['navigate']);
 
-    await TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      providers: [MockProvider(UserService, userServiceMock), MockProvider(Router, routerMock)],
-      imports: [CommonModule, FormsModule, NoopAnimationsModule, MatInputModule, MatButtonModule, MatFormFieldModule],
-    });
+    return MockBuilder(LoginComponent, LoginModule)
+      .keep(FormsModule)
+      .provide(MockProvider(UserService, userServiceMock))
+      .provide(MockProvider(Router, routerMock));
+  });
 
-    fixture = TestBed.createComponent(LoginComponent);
+  beforeEach(() => {
+    fixture = MockRender(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
